daf7a191e262aacd105ccaee9d2d3b91fdfa4f6b
고명성 <audtjd9080@naver.com>
2024-04-17 17:23:41 +0900

    🎉 begin: SmartFactory 백엔드 프로젝트 시작:)
    - build.gradle 파일에 다음 의존성들을 추가:
      - Spring Boot Starter Web, Actuator, Data Redis, Kafka
      - MariaDB JDBC 클라이언트
      - Lombok 라이브러리 및 Configuration Processor

---
+++ be/smartfactory/src/main/java/samsung/smart/factory/SmartFactoryApplication.java
@@ -0,0 +1,13 @@
+package samsung.smart.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFactoryApplication.class, args);
+    }
+
+}
---
+++ be/smartfactory/src/test/java/samsung/smart/factory/SmartFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package samsung.smart.factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
