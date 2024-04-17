f61c2e217a1b7ee13acfb387797d3fb4e7a63900
엄진식 <ladiescheong@naver.com>
2024-04-17 10:03:17 +0900

    ✨ feat: Eslint & Prettier 기반 코드 포맷팅 시스템 구축
    코드 통일성을 위해 Airbnb 코드 컨벤션 기반으로 포맷팅 시스템 구축

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    'airbnb',
+    'airbnb/typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './frontend/tsconfig.json',
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto', semicolon: 'true' }],
+  },
+};
