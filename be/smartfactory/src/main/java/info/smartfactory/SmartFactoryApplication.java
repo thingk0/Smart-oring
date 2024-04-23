89074a5ad8731da85a8736707ec0e1e2b16120fe
hyejinKim1 <201802084@o.cnu.ac.kr>
2024-04-23 13:27:53 +0900

    ♻️ refactor: 패키지명 수정 및 JPA dependency 추가

--- be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
+++ be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
@@ -1,4 +1,4 @@
-package samsung.smart.factory;
+package info.smartfactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- be/smartfactory/src/test/java/info/smartfactory/SmartFactoryApplicationTests.java
+++ be/smartfactory/src/test/java/info/smartfactory/SmartFactoryApplicationTests.java
@@ -1,4 +1,4 @@
-package samsung.smart.factory;
+package info.smartfactory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
