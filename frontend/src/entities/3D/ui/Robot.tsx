74431f9a039fd1fc848108004e62d1a9ff61d96e
엄진식 <ladiescheong@naver.com>
2024-04-26 13:36:30 +0900

    🐛 fix: robot position api 모킹 로직 및 polling 안되는 현상 수정

--- frontend/src/entities/3D/ui/Robot.tsx
+++ frontend/src/entities/3D/ui/Robot.tsx
@@ -1,21 +1,34 @@
+import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { useGLTF } from '@react-three/drei';
 import { getRobotPosition } from '../../../shared/api';
+import { Point2D, robotData } from '../../../shared/types';
+import { Vector3 } from 'three';
+
+const convertPosition = (position: Point2D): Vector3 => {
+  return new Vector3(position[0], 0, position[1]);
+};
 
 function Robot() {
-  const query = useQuery({
+  let query = useQuery({
     queryKey: ['robotPosition'],
     queryFn: getRobotPosition,
-    staleTime: 1000,
+    refetchInterval: 1000,
   });
+
+  const forklift = useGLTF('./models/Forklift01.glb');
+
+  useEffect(() => {
+    console.log('query changed', query.data);
+  }, [query]);
 
   return (
     <>
-      {query.data?.map(p => {
+      {query.data?.map((p: robotData) => {
         return (
-          <mesh>
-            <planeGeometry />
-            <meshBasicMaterial color="pink" />
-          </mesh>
+          <object3D key={p.id} position={convertPosition(p.position)}>
+            <primitive object={forklift.scene} />
+          </object3D>
         );
       })}
     </>
--- frontend/src/shared/api/index.ts
+++ frontend/src/shared/api/index.ts
@@ -1,15 +1,9 @@
 import axios from 'axios';
-
-type robotData = {
-  position: number[];
-  battery: number;
-  id: string;
-  mission: string;
-};
+import { robotData } from '../types';
 
 const getRandomPosition = (position: number[]) => {
   const tmp = Math.round(Math.random());
-  return [position[0] + tmp, position[1] + ~tmp];
+  return [position[0] + tmp, position[1] + (tmp ^ 1)];
 };
 
 // robot movement mocking-api
@@ -27,3 +21,29 @@ instanceAPI.interceptors.response.use(res => {
 
 export const getRobotPosition = () =>
   instanceAPI.get(`http://localhost:3001/robot`).then(res => res.data);
+
+//   {
+//     "status": "success",
+//     "fetchStatus": "fetching",
+//     "isPending": false,
+//     "isSuccess": true,
+//     "isError": false,
+//     "isInitialLoading": false,
+//     "isLoading": false,
+//     "data": [],
+//     "dataUpdatedAt": 1714096041610,
+//     "error": null,
+//     "errorUpdatedAt": 0,
+//     "failureCount": 0,
+//     "failureReason": null,
+//     "errorUpdateCount": 0,
+//     "isFetched": true,
+//     "isFetchedAfterMount": true,
+//     "isFetching": true,
+//     "isRefetching": true,
+//     "isLoadingError": false,
+//     "isPaused": false,
+//     "isPlaceholderData": false,
+//     "isRefetchError": false,
+//     "isStale": true
+// }
