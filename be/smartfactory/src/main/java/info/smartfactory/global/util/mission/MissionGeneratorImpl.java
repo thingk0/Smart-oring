4372d31c02ac344d016d1110b7436f47036327e3
Hgstudio <tndms0308@naver.com>
2024-04-29 13:47:56 +0900

    ✨ feat: MissionGeneratorImpl 생성

---
+++ be/smartfactory/src/main/java/info/smartfactory/global/util/mission/MissionGeneratorImpl.java
@@ -0,0 +1,11 @@
+package info.smartfactory.global.util.mission;
+
+import info.smartfactory.domain.mission.entity.Mission;
+
+public class MissionGeneratorImpl implements MissionGenerator {
+
+    @Override
+    public Mission generateRandomMission() {
+        return null;
+    }
+}
