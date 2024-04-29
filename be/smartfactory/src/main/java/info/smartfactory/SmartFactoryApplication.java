39c6780714926ec924a7f6f8cb80eed175d39f6f
고명성 <audtjd9080@naver.com>
2024-04-29 13:30:36 +0900

    🔧 config: Spring 구성 파일 수정 및 JPA 감사 기능 활성화
    - Spring configuration 파일에서 'optional:' 접두어 제거하여 설정 로드 방식 단순화.
    - @EnableJpaAuditing 어노테이션 추가로 JPA 감사 기능 활성화:
      - SmartFactoryApplication 클래스에 추가하여 엔티티의 생성 및 수정 시간 자동 기록 활성화.

--- be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
+++ be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
@@ -2,7 +2,9 @@ package info.smartfactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SmartFactoryApplication {
 
