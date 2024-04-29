3a74b730533f2fb8ec3cff70cde8f59cfc0333b6
고명성 <audtjd9080@naver.com>
2024-04-29 14:07:31 +0900

    ☀️ add: AmrStatus 열거형 추가
    - AMR 상태를 정의하는 열거형 AmrStatus 추가:
      - IDLE, MOVING, CHARGING, PICKUP, DELIVERY, ERROR 상태 포함.
    - Amr 엔티티의 상태 관리를 명확하고 효율적으로 수행.

---
+++ be/smartfactory/src/main/java/info/smartfactory/domain/amr/constant/AmrStatus.java
@@ -0,0 +1,10 @@
+package info.smartfactory.domain.amr.constant;
+
+public enum AmrStatus {
+    IDLE,           // 대기 상태
+    MOVING,         // 이동 중
+    CHARGING,       // 충전 중
+    PICKUP,         // 물류 픽업 중
+    DELIVERY,       // 물류 배송 중
+    ERROR           // 에러 발생
+}
