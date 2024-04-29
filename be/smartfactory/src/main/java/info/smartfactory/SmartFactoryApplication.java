45821b8e4436827cb3e12c11c4ae7432b2daa636
Hgstudio <tndms0308@naver.com>
2024-04-29 17:48:46 +0900

    ✨ feat: EnableScheduling annotation추가

--- be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
+++ be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 public class SmartFactoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartFactoryApplication.class, args);
     }
+
 }
