095d10a02ef3aa04c710336669a122cf2d48ddbf
고명성 <audtjd9080@naver.com>
2024-04-30 13:35:29 +0900

    🔧 config: 애플리케이션 설정에 Kafka 활성화 추가
    - `@EnableKafka` 어노테이션 추가하여 애플리케이션 레벨에서 Kafka 리스너를 활성화

--- be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
+++ be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
@@ -3,7 +3,9 @@ package info.smartfactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @EnableJpaAuditing
 @SpringBootApplication
 public class SmartFactoryApplication {
@@ -11,5 +13,4 @@ public class SmartFactoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartFactoryApplication.class, args);
     }
-
 }
