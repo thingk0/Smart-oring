944d64074707bc0924c3fa96ecfb0b4bdeefec19
Hgstudio <tndms0308@naver.com>
2024-05-02 04:59:25 +0900

    ✨ feat: MissionGeneratorImplTest 생성

---
+++ be/smartfactory/src/test/java/info/smartfactory/global/util/mission/MissionGeneratorImplTest.java
@@ -0,0 +1,61 @@
+package info.smartfactory.global.util.mission;
+
+import info.smartfactory.domain.mission.entity.Mission;
+import info.smartfactory.domain.node.entity.Destination;
+import info.smartfactory.domain.node.entity.Node;
+import info.smartfactory.domain.node.entity.Storage;
+import info.smartfactory.domain.node.entity.StorageDirection;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
+@Transactional
+class MissionGeneratorImplTest {
+    //@Autowired
+    MissionGeneratorImpl missionGenerator = new MissionGeneratorImpl();
+    
+    
+    @Test
+    public void 미션생성() throws Exception {
+        // Given
+        List<Storage> storageList = new ArrayList<>();
+
+        Storage storage1 = Storage.createStorage(0,0,StorageDirection.East);
+        Storage storage2 = Storage.createStorage(1, 1, StorageDirection.South);
+        Storage storage3 = Storage.createStorage(2, 2, StorageDirection.North);
+        Storage storage4 = Storage.createStorage(3, 3, StorageDirection.North);
+        Storage storage5 = Storage.createStorage(4, 4, StorageDirection.North);
+        Storage storage6 = Storage.createStorage(5, 5, StorageDirection.North);
+
+        storageList.add(storage1);
+        storageList.add(storage2);
+        storageList.add(storage3);
+        storageList.add(storage4);
+        storageList.add(storage5);
+        storageList.add(storage6);
+
+        List<Destination> destinationList = new ArrayList<>();
+
+        Destination destination1 = Destination.createDestination(3, 3);
+        Destination destination2 = Destination.createDestination(4, 4);
+        Destination destination3 = Destination.createDestination(5, 5);
+
+        destinationList.add(destination1);
+        destinationList.add(destination2);
+        destinationList.add(destination3);
+
+        // When
+        Mission mission = missionGenerator.generateRandomMission(3, destinationList, storageList);
+
+        // Then
+
+    }
+
+}
