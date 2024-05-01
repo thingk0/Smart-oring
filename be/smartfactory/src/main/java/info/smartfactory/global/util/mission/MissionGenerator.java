52eb07eb5089032c439d3b2172cd554335c859c1
Hgstudio <tndms0308@naver.com>
2024-05-02 04:51:32 +0900

    ♻ refactor: generateRandomMission 파라미터 수정

--- be/smartfactory/src/main/java/info/smartfactory/global/util/mission/MissionGenerator.java
+++ be/smartfactory/src/main/java/info/smartfactory/global/util/mission/MissionGenerator.java
@@ -1,10 +1,14 @@
 package info.smartfactory.global.util.mission;
 
 import info.smartfactory.domain.mission.entity.Mission;
+import info.smartfactory.domain.node.entity.Destination;
 import info.smartfactory.domain.node.entity.Node;
+import info.smartfactory.domain.node.entity.Storage;
+
+import java.util.List;
 
 public interface MissionGenerator {
-    Mission generateRandomMission(int submissionNum, Node[] destinations, Node[] storages); //랜덤으로 미션 생성 후 DB에 저장하고 Mission 반환
+    Mission generateRandomMission(int submissionNum, List<Destination> destinations, List<Storage> storages); //랜덤으로 미션 생성 후 DB에 저장하고 Mission 반환
 
 }
 
