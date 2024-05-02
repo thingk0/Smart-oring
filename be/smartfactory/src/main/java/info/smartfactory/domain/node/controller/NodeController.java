062a759cc228acc218198d5b11a8c46e1afa5b2d
고명성 <audtjd9080@naver.com>
2024-05-02 17:57:01 +0900

    ♻️ refactor: 응답 방식 통일 및 메서드 명확화
    - MapDto를 MapData로 변경하지 않고 기존 이름 유지
    - addMapData, clearMapData 메서드 명칭을 명확화

--- be/smartfactory/src/main/java/info/smartfactory/domain/node/controller/NodeController.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/node/controller/NodeController.java
@@ -1,38 +1,42 @@
 package info.smartfactory.domain.node.controller;
 
-import info.smartfactory.domain.node.dto.MapDto;
-import info.smartfactory.domain.node.entity.Node;
+import info.smartfactory.domain.node.dto.MapData;
+import info.smartfactory.domain.node.dto.request.MapAddRequest;
 import info.smartfactory.domain.node.service.NodeService;
 import info.smartfactory.global.result.ResultResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import info.smartfactory.domain.node.dto.request.AddMapRequest;
-import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class NodeController {
+
     private final NodeService nodeService;
 
     @GetMapping("/map")
-    public ResponseEntity<ResultResponse<MapDto>> mapData() {
-        MapDto mapData = nodeService.getMapData();
+    public ResponseEntity<?> mapData() {
+        MapData mapData = nodeService.getMapData();
         return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), mapData));
     }
 
     @DeleteMapping("/map/delete")
-    public ResponseEntity<ResultResponse<MapDto>> deleteMapData() {
-        nodeService.deleteMapData();
+    public ResponseEntity<?> deleteMapData() {
+        nodeService.clearMapData();
         return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString()));
     }
 
     @PostMapping("/map/add")
-    public ResponseEntity<ResultResponse<Node>> addMapData(@RequestBody @Valid AddMapRequest request) {
-        nodeService.addMapData(request);
+    public ResponseEntity<?> addMapData(@RequestBody @Valid MapAddRequest request) {
+        nodeService.addNode(request);
         return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString()));
     }
 
