1f8d5175c9ba6f41c6aa4e571f223c299c684556
Hgstudio <tndms0308@naver.com>
2024-05-02 14:11:19 +0900

    ✨ feat: DestinationRepository getDestination() 생성

--- be/smartfactory/src/main/java/info/smartfactory/domain/node/repository/DestinationRepository.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/node/repository/DestinationRepository.java
@@ -1,7 +1,13 @@
-package info.smartfactory.domain.node.repository;
+package info.smartfactory.domain.node.Repository;
 
 import info.smartfactory.domain.node.entity.Destination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface DestinationRepository extends JpaRepository<Destination, Long> {
+import java.util.List;
+
+public interface DestinationRepository extends JpaRepository<Destination, Integer> {
+    @Query("SELECT d FROM Destination d")
+    List<Destination> getDestination();
+
 }
