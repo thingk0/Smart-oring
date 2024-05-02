7dd8f914829280e6b96025eb5522565659719faa
엄진식 <ladiescheong@naver.com>
2024-05-02 15:11:54 +0900

    💩 bad: 질문 공유를 위한 코드 업로드

--- frontend/src/entities/3D/ui/InstancedRobot.tsx
+++ frontend/src/entities/3D/ui/InstancedRobot.tsx
@@ -1,27 +1,10 @@
-import { useEffect, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useGLTF } from '@react-three/drei';
-import { Matrix4, Vector3 } from 'three';
-import gsap from 'gsap';
-
+import { useMemo } from 'react';
 import { getRobotPosition } from '../../../shared/api';
-import { Point2D, robotData } from '../../../shared/types';
+import RobotModel from './RobotModel';
 
-// utils
-const convertPosition = (position: Point2D): Vector3 => {
-  return new Vector3(position[1], 0, position[0]);
-};
-
-const arr = [0, Math.PI / 2, 0, Math.PI * 1.5, Math.PI];
-
-const getRotationIndex = (before: robotData, current: robotData) => {
-  const y = before.position[0] - current.position[0];
-  const x = before.position[1] - current.position[1];
-  const rotationIndex = 2 * y + x + 2;
-
-  return [y, x, rotationIndex];
-};
-
+// main component
 function InstancedRobot() {
   const { data, isPending } = useQuery({
     queryKey: ['robotPosition'],
@@ -29,42 +12,22 @@ function InstancedRobot() {
     // refetchInterval: 1000,
   });
 
-  const model = useGLTF('./models/AGV.glb');
-
-  const [beforePositions, setBeforePositions] = useState([]);
-  const forklifts = useRef();
-
-  useEffect(() => {
-    console.log(forklifts.current);
-
-    // calculate direction
-    // beforePositions?.forEach((before: robotData, index: number) => {
-    //   const [y, x, rotationIndex] = getRotationIndex(before, data[index]);
-    //   const direction = arr[rotationIndex];
-    //   // move forklifts position
-    //   gsap.to(forklifts.current[index].position, {
-    //     duration: 1,
-    //     ease: 'none',
-    //     x: data[index].position[1] + x,
-    //     z: data[index].position[0] + y,
-    //     onComplete: () => {
-    //       // rotate forklifts
-    //       forklifts.current[index].rotation.y = direction;
-    //     },
-    //   });
-    // });
-    // // update state
-    // setBeforePositions(data);
-  }, [data]);
+  const { scene } = useGLTF('./models/AGV.glb');
+  const model = useMemo(() => {
+    return scene;
+  }, [scene]);
 
   return (
-    <group>
-      {!isPending && (
-        <instancedMesh ref={forklifts} count={data?.length}>
-          <primitive object={model.scene.clone()} />
-        </instancedMesh>
-      )}
-    </group>
+    <>
+      <instancedMesh count={data?.length}>
+        {!isPending &&
+          data?.map((robot: any, index: number) => {
+            return (
+              <RobotModel key={index} position={robot.position} scene={model} />
+            );
+          })}
+      </instancedMesh>
+    </>
   );
 }
 
