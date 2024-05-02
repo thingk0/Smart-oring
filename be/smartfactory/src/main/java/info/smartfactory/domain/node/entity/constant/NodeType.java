f66d036d40f2da2f7ed8ac1776ae226b5c6c86c6
고명성 <audtjd9080@naver.com>
2024-05-02 20:43:27 +0900

    ☀️ add: NodeType 상수 클래스 추가
    - NodeType 클래스에 노드 타입을 상수로 정의하여 코드 전반에 걸쳐 사용
    - 'NODE', 'CHARGER', 'DESTINATION', 'STORAGE' 등의 노드 타입을 상수로 관리하여 타입 안정성 및 가독성 향상

---
+++ be/smartfactory/src/main/java/info/smartfactory/domain/node/entity/constant/NodeType.java
@@ -0,0 +1,10 @@
+package info.smartfactory.domain.node.entity.constant;
+
+public class NodeType {
+
+    public static final String NODE = "NODE";
+    public static final String CHARGER = "CHARGER";
+    public static final String DESTINATION = "DESTINATION";
+    public static final String STORAGE = "STORAGE";
+
+}
