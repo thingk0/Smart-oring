d859db6ad4f82d070badcdb4836663975058db36
고명성 <audtjd9080@naver.com>
2024-05-02 17:50:14 +0900

    ♻️ refactor: 노드 관련 엔티티의 패키지 구조 및 생성자 로직 변경
    - Destination, Charger, Storage 클래스를 'entity.type' 패키지로 이동
    - EntranceDirection과 NodeType 상수로 변경하여 관리 효율성 향상
    - 각 클래스의 생성자를 private로 변경하고 from 메서드를 통해 인스턴스 생성하도록 수정
      -> MapAddRequest를 사용하여 노드 객체를 초기화할 수 있도록 변경
    - updateMap 메서드를 각 노드 타입에 맞게 재정의하여 맵 데이터 업데이트 방식을 명확화
    - 기존 Direction 열거형 삭제 및 EntranceDirection 사용으로 일관성 강화

--- be/smartfactory/src/main/java/info/smartfactory/domain/node/entity/Direction.java
+++
@@ -1,6 +0,0 @@
-package info.smartfactory.domain.node.entity;
-
-public enum Direction {
-    East, West, South, North
-    // 동 서 남 북
-}
---
+++ be/smartfactory/src/main/java/info/smartfactory/domain/node/entity/type/Charger.java
@@ -0,0 +1,37 @@
+package info.smartfactory.domain.node.entity.type;
+
+import info.smartfactory.domain.node.dto.request.MapAddRequest;
+import info.smartfactory.domain.node.entity.Node;
+import info.smartfactory.domain.node.entity.constant.EntranceDirection;
+import info.smartfactory.domain.node.entity.constant.NodeType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@PrimaryKeyJoinColumn(name = "node_id")
+@DiscriminatorValue(NodeType.CHARGER)
+@Table(name = "charger")
+public class Charger extends Node {
+
+    private Charger(Integer x, Integer y, EntranceDirection entranceDirection) {
+        super(x, y, entranceDirection);
+    }
+
+    public static Charger from(MapAddRequest addRequest) {
+        return new Charger(addRequest.xCoordinate(), addRequest.yCoordinate(),
+                           EntranceDirection.fromValue(addRequest.direction()));
+    }
+
+    @Override
+    public void updateMap(String[][][] map) {
+        map[getXCoordinate()][getYCoordinate()][0] = NodeType.CHARGER;
+        map[getXCoordinate()][getYCoordinate()][1] = getEntranceDirection().name();
+    }
+}
