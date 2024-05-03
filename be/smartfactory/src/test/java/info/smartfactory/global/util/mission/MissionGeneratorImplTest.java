5144ad984d5c9c2254394a789ec5f6da6bd93b0a
Hgstudio <tndms0308@naver.com>
2024-05-03 16:35:13 +0900

    ♻️ refactor: MissionGeneratorImplTest 주석처리

--- be/smartfactory/src/test/java/info/smartfactory/global/util/mission/MissionGeneratorImplTest.java
+++ be/smartfactory/src/test/java/info/smartfactory/global/util/mission/MissionGeneratorImplTest.java
@@ -1,57 +1,48 @@
 package info.smartfactory.global.util.mission;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.transaction.annotation.Transactional;
-
-import info.smartfactory.domain.mission.entity.Mission;
-import info.smartfactory.domain.node.entity.type.Destination;
-import info.smartfactory.domain.node.entity.constant.EntranceDirection;
-import info.smartfactory.domain.node.entity.type.Storage;
 
 //@SpringBootTest
 @Transactional
 class MissionGeneratorImplTest {
     //@Autowired
-    MissionGeneratorImpl missionGenerator = new MissionGeneratorImpl();
-    
-    
-    @Test
-    public void 미션생성() throws Exception {
-        // Given
-        List<Storage> storageList = new ArrayList<>();
-
-        Storage storage1 = Storage.createStorage(0,0, EntranceDirection.East);
-        Storage storage2 = Storage.createStorage(1, 1, EntranceDirection.South);
-        Storage storage3 = Storage.createStorage(2, 2, EntranceDirection.North);
-        Storage storage4 = Storage.createStorage(3, 3, EntranceDirection.North);
-        Storage storage5 = Storage.createStorage(4, 4, EntranceDirection.North);
-        Storage storage6 = Storage.createStorage(5, 5, EntranceDirection.North);
-
-        storageList.add(storage1);
-        storageList.add(storage2);
-        storageList.add(storage3);
-        storageList.add(storage4);
-        storageList.add(storage5);
-        storageList.add(storage6);
-
-        List<Destination> destinationList = new ArrayList<>();
-
-        Destination destination1 = Destination.createDestination(6, 6, EntranceDirection.East);
-        Destination destination2 = Destination.createDestination(7, 7, EntranceDirection.East);
-        Destination destination3 = Destination.createDestination(8, 8, EntranceDirection.East);
-
-        destinationList.add(destination1);
-        destinationList.add(destination2);
-        destinationList.add(destination3);
-
-        // When
-        Mission mission = missionGenerator.generateRandomMission(3, destinationList, storageList);
-
-        // Then
-
-    }
+//    MissionGeneratorImpl missionGenerator = new MissionGeneratorImpl();
+//
+//
+//    @Test
+//    public void 미션생성() throws Exception {
+//        // Given
+//        List<Storage> storageList = new ArrayList<>();
+//
+//        Storage storage1 = Storage.createStorage(0,0, EntranceDirection.East);
+//        Storage storage2 = Storage.createStorage(1, 1, EntranceDirection.South);
+//        Storage storage3 = Storage.createStorage(2, 2, EntranceDirection.North);
+//        Storage storage4 = Storage.createStorage(3, 3, EntranceDirection.North);
+//        Storage storage5 = Storage.createStorage(4, 4, EntranceDirection.North);
+//        Storage storage6 = Storage.createStorage(5, 5, EntranceDirection.North);
+//
+//        storageList.add(storage1);
+//        storageList.add(storage2);
+//        storageList.add(storage3);
+//        storageList.add(storage4);
+//        storageList.add(storage5);
+//        storageList.add(storage6);
+//
+//        List<Destination> destinationList = new ArrayList<>();
+//
+//        Destination destination1 = Destination.createDestination(6, 6, EntranceDirection.East);
+//        Destination destination2 = Destination.createDestination(7, 7, EntranceDirection.East);
+//        Destination destination3 = Destination.createDestination(8, 8, EntranceDirection.East);
+//
+//        destinationList.add(destination1);
+//        destinationList.add(destination2);
+//        destinationList.add(destination3);
+//
+//        // When
+//        Mission mission = missionGenerator.generateRandomMission(3, destinationList, storageList);
+//
+//        // Then
+//
+//    }
 
 }
