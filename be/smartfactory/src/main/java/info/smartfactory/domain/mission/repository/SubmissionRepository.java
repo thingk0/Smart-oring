7dd2e441dabbfe71be16b68ba7166836c276dd16
Hgstudio <tndms0308@naver.com>
2024-05-04 21:23:18 +0900

    ✨ feat: SubmissionRepository findByMissionIdWithNodes 파라미터 에노테이션 추가

--- be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
@@ -3,6 +3,7 @@ package info.smartfactory.domain.mission.repository;
 import info.smartfactory.domain.mission.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface SubmissionRepository extends JpaRepository<Submission, Long> {
     @Query("""
             SELECT s
             FROM Submission s JOIN FETCH s.arriveNode n
-            WHERE s.arriveNode.id = :missionId
+            WHERE s.mission.id = :missionId
             """)
-    List<Submission> findByMissionIdWithNodes(Long missionId);
+    List<Submission> findByMissionIdWithNodes(@Param("missionId") Long missionId);
 }
