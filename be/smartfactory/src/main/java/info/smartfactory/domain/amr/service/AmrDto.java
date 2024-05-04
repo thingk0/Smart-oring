762434150226417fca1ace822e3f1974a527c43a
Hgstudio <tndms0308@naver.com>
2024-05-05 03:12:57 +0900

    ♻ refactor: AmrDto amrStatus 삭제

--- be/smartfactory/src/main/java/info/smartfactory/domain/amr/service/AmrDto.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/amr/service/AmrDto.java
@@ -1,6 +1,6 @@
 package info.smartfactory.domain.amr.service;
 
-import info.smartfactory.domain.amr.constant.AmrStatus;
+import info.smartfactory.domain.history.constant.AmrStatus;
 import info.smartfactory.domain.amr.entity.Amr;
 import info.smartfactory.domain.mission.entity.Mission;
 import lombok.Data;
@@ -10,14 +10,12 @@ public class AmrDto {
 	private Long id;
 	private Mission mission;
 	private String amrCode;
-	private AmrStatus amrStatus;
 
 	static AmrDto from(Amr amr) {
 		AmrDto amrDto = new AmrDto();
 		amrDto.setId(amr.getId());
 		amrDto.setMission(amr.getMission());
 		amrDto.setAmrCode(amr.getAmrCode());
-		amrDto.setAmrStatus(amr.getAmrStatus());
 		return amrDto;
 	}
 }
