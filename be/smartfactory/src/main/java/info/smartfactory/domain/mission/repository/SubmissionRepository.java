5fa014e7e858caee3fdf9944e8bd54cb301e80f0
Hgstudio <tndms0308@naver.com>
2024-05-04 19:01:37 +0900

    ✨ feat: SubmissionRepository Submission 조회 생성

--- be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
@@ -2,7 +2,16 @@ package info.smartfactory.domain.mission.repository;
 
 import info.smartfactory.domain.mission.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
+    @Query("""
+            SELECT s
+            FROM Submission s JOIN FETCH s.arriveNode n
+            WHERE s.arriveNode.id = :missionId
+            """)
+    List<Submission> findByMissionIdWithNodes(Long missionId);
 }
