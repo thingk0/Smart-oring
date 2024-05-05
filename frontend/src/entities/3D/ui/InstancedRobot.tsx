ea1fedced62b3a3582f309e96b4c9d60ef83b207
엄진식 <ladiescheong@naver.com>
2024-05-05 15:20:46 +0900

    ♻️ refactor: type, util function 이동

--- frontend/src/entities/3D/ui/InstancedRobot.tsx
+++ frontend/src/entities/3D/ui/InstancedRobot.tsx
@@ -1,34 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { Merged, useGLTF } from '@react-three/drei';
 import { useMemo, useState, useRef, useEffect } from 'react';
-import { getRobotPosition } from '../../../shared/api';
-import RobotModel from './RobotModel';
-import { Object3D } from 'three';
-import { robotData } from '../../../shared/types';
 import { gsap } from 'gsap';
 
-// type
-export type TRobot = {
-  geo_aluminium_3: Object3D;
-  geo_black_7: Object3D;
-  geo_black_matte_1: Object3D;
-  geo_black_smoke_glass_8: Object3D;
-  geo_light_cyan_1: Object3D;
-  geo_orange_1: Object3D;
-  geo_rubber_6: Object3D;
-};
-
-// utils
-
-const arr = [0, Math.PI / 2, 0, Math.PI * 1.5, Math.PI];
-
-const getRotationIndex = (before: robotData, current: robotData) => {
-  const y = before.position[0] - current.position[0];
-  const x = before.position[1] - current.position[1];
-  const rotationIndex = 2 * y + x + 2;
-
-  return [y, x, rotationIndex];
-};
+import { getRobotPosition } from '../../../shared/api';
+import RobotModel from './RobotModel';
+import { robotData } from '../../../shared/types';
+import { getRotationIndex } from '../../../shared/lib';
 
 // main component
 function InstancedRobot() {
@@ -44,8 +22,7 @@ function InstancedRobot() {
   useEffect(() => {
     // calculate direction
     beforePositions?.forEach((before: robotData, index: number) => {
-      const [y, x, rotationIndex] = getRotationIndex(before, data[index]);
-      const direction = arr[rotationIndex];
+      const [y, x, radian] = getRotationIndex(before, data[index]);
 
       // move forklifts position
       gsap.to(forklifts.current?.children[index].position, {
@@ -55,7 +32,7 @@ function InstancedRobot() {
         z: data[index].position[0] + y,
         onComplete: () => {
           // rotate forklifts
-          forklifts.current.children[index].rotation.y = direction;
+          forklifts.current.children[index].rotation.y = radian;
         },
       });
     });
@@ -77,8 +54,6 @@ function InstancedRobot() {
     }),
     [nodes]
   );
-
-  // console.log(forklifts.current?.children);
 
   return (
     <group ref={forklifts}>
