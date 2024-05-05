67aea36b9afb2e32ac6f0a5b19ab9fede7d48d5a
hyejinKim1 <201802084@o.cnu.ac.kr>
2024-05-05 17:28:53 +0900

    🚑 Hotfix: AMR 상태 변경

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
@@ -1,10 +1,10 @@
 package info.smartfactory.domain.history.entity.constant;
 
 public enum AmrStatus {
-    IDLE,           // 대기 상태
-    MOVING,         // 이동 중
+    MOVING,         // 미션 수행 중
+    STOPPED,        // 미션 수행 중 멈춤
     CHARGING,       // 충전 중
-    PICKUP,         // 물류 픽업 중
-    DELIVERY,       // 물류 배송 중
-    ERROR           // 에러 발생
+    ERROR,          // 에러 발생
+    DISCHARGING     // 방전
+
 }
