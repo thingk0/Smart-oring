234eb28d9b2b6ee3e97f94a121bca3788e3488e1
엄진식 <ladiescheong@naver.com>
2024-05-05 15:16:10 +0900

    ✨ feat: merged로 구현된 로봇에 기존 애니메이션 부착

--- frontend/src/entities/3D/ui/InstancedRobot.tsx
+++ frontend/src/entities/3D/ui/InstancedRobot.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { Merged, useGLTF } from '@react-three/drei';
-import { useMemo } from 'react';
+import { useMemo, useState, useRef, useEffect } from 'react';
 import { getRobotPosition } from '../../../shared/api';
 import RobotModel from './RobotModel';
 import { Object3D } from 'three';
+import { robotData } from '../../../shared/types';
+import { gsap } from 'gsap';
 
 // type
 export type TRobot = {
@@ -16,13 +18,51 @@ export type TRobot = {
   geo_rubber_6: Object3D;
 };
 
+// utils
+
+const arr = [0, Math.PI / 2, 0, Math.PI * 1.5, Math.PI];
+
+const getRotationIndex = (before: robotData, current: robotData) => {
+  const y = before.position[0] - current.position[0];
+  const x = before.position[1] - current.position[1];
+  const rotationIndex = 2 * y + x + 2;
+
+  return [y, x, rotationIndex];
+};
+
 // main component
 function InstancedRobot() {
   const { data, isPending } = useQuery({
     queryKey: ['robotPosition'],
     queryFn: getRobotPosition,
-    // refetchInterval: 1000,
+    refetchInterval: 1000,
   });
+
+  const [beforePositions, setBeforePositions] = useState([]);
+  const forklifts = useRef(null);
+
+  useEffect(() => {
+    // calculate direction
+    beforePositions?.forEach((before: robotData, index: number) => {
+      const [y, x, rotationIndex] = getRotationIndex(before, data[index]);
+      const direction = arr[rotationIndex];
+
+      // move forklifts position
+      gsap.to(forklifts.current?.children[index].position, {
+        duration: 1,
+        ease: 'none',
+        x: data[index].position[1] + x,
+        z: data[index].position[0] + y,
+        onComplete: () => {
+          // rotate forklifts
+          forklifts.current.children[index].rotation.y = direction;
+        },
+      });
+    });
+
+    // update state
+    setBeforePositions(data);
+  }, [data]);
 
   const { nodes } = useGLTF('./models/AGV.glb');
   const instances: TRobot = useMemo(
@@ -38,17 +78,19 @@ function InstancedRobot() {
     [nodes]
   );
 
+  // console.log(forklifts.current?.children);
+
   return (
-    <>
+    <group ref={forklifts}>
       {!isPending &&
-        data?.map((robot: any, index: number) => {
+        data?.map((_: any, index: number) => {
           return (
             <Merged meshes={instances} key={index}>
               {(instances: TRobot) => <RobotModel instances={instances} />}
             </Merged>
           );
         })}
-    </>
+    </group>
   );
 }
 
