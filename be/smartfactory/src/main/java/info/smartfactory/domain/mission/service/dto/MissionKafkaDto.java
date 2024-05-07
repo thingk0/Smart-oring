51a296653e2cef10ff2ef6861fda4617fc0d8130
Hgstudio <tndms0308@naver.com>
2024-05-07 10:59:15 +0900

    ✨ feat: MissionKafkaDto 생성

---
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/service/dto/MissionKafkaDto.java
@@ -0,0 +1,12 @@
+package info.smartfactory.domain.mission.service.dto;
+
+import info.smartfactory.domain.mission.entity.Mission;
+
+import java.time.LocalDateTime;
+
+/**
+ * DTO for {@link Mission}
+ */
+public record MissionKafkaDto(Long id, LocalDateTime missionStartedAt, LocalDateTime missionFinishedAt,
+                              LocalDateTime missionEstimatedTime) {
+}
