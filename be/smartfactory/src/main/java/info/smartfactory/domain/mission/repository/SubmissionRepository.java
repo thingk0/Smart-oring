e310d63e639c3e3d7cbd3d3ce811061ba638d9b8
Hgstudio <tndms0308@naver.com>
2024-05-07 16:05:01 +0900

    ♻️ refactor: findByMssionIdWithNodes 쿼리 반환형 수정

--- be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/repository/SubmissionRepository.java
@@ -1,11 +1,10 @@
 package info.smartfactory.domain.mission.repository;
 
+import info.smartfactory.domain.mission.entity.Mission;
 import info.smartfactory.domain.mission.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
@@ -16,5 +15,5 @@ public interface SubmissionRepository extends JpaRepository<Submission, Long> {
             JOIN FETCH s.arriveNode n
             WHERE s.mission.id = :missionId
             """)
-    List<Submission> findByMissionIdWithNodes(@Param("missionId") Long missionId);
+    Mission findByMissionIdWithNodes(@Param("missionId") Long missionId);
 }
