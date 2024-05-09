24355d946a1b559e0263ff1dbe6eb67bc8320586
김예지 <fpssj@naver.com>
2024-05-09 14:34:48 +0900

    ✨ feat: 로봇 클릭시 카메라 시점 변경 기능 추가

--- frontend/src/entities/3D/ui/RobotModel.tsx
+++ frontend/src/entities/3D/ui/RobotModel.tsx
@@ -1,17 +1,34 @@
+import { ThreeEvent, useFrame } from '@react-three/fiber';
 import { TRobot } from '../../../shared/types';
-
+import { useState } from 'react';
+import * as THREE from 'three';
 // props
 type RobotModelProps = {
   instances: TRobot;
+  name: string;
 };
 
 // main function
-function RobotModel({ instances, ...props }: RobotModelProps) {
+function RobotModel({ instances, name, ...props }: RobotModelProps) {
   // console.log(instances);
 
+  const [isFPV, setIsFPV] = useState(false);
+  useFrame(state => {
+    if (!isFPV) return;
+    const target = new THREE.Vector3();
+    const robot = state.scene.getObjectByName(name);
+    // getWorldPosition으로 target에 robot의 위치를 저장한다. (헷갈림 주의)
+    robot?.children[0].getWorldPosition(target);
+    target.y = 2;
+    state.camera.position.copy(target);
+  });
   return (
     <group {...props}>
-      <group>
+      <group
+        name={name}
+        onClick={() => setIsFPV(true)}
+        onPointerMissed={() => setIsFPV(false)}
+      >
         <pointLight color="#00afff" intensity={10} />
         <instances.geo_aluminium_3 />
         <instances.geo_black_7 />
