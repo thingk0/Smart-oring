3b23a9bed805952f12ef2bf7b0d3f59dffd31bef
bluepeng9 <bluepeng9@gmail.com>
2024-05-09 11:25:44 +0900

    ✨ feat: Node에 entrance 추가

--- simulator/domain/node.py
+++ simulator/domain/node.py
@@ -3,9 +3,10 @@ from enum import Enum
 
 class Node:
 
-    def __init__(self, xcoordinate, ycoordinate):
+    def __init__(self, xcoordinate, ycoordinate, entranceDirection):
         self.x_coordinate = xcoordinate
         self.y_coordinate = ycoordinate
+        self.entranceDirection = entranceDirection
 
     def __str__(self):
         return f"x: {self.x_coordinate}, y: {self.y_coordinate}"
@@ -14,20 +15,17 @@ class Node:
 class Charger(Node):
 
     def __init__(self, xcoordinate, ycoordinate, entranceDirection):
-        super().__init__(xcoordinate, ycoordinate)
-        self.entranceDirection: EntranceDirection = entranceDirection
+        super().__init__(xcoordinate, ycoordinate, entranceDirection)
 
 
 class Storage(Node):
     def __init__(self, xcoordinate, ycoordinate, entranceDirection):
-        super().__init__(xcoordinate, ycoordinate)
-        self.entranceDirection: EntranceDirection = entranceDirection
+        super().__init__(xcoordinate, ycoordinate, entranceDirection)
 
 
 class Destination(Node):
     def __init__(self, xcoordinate, ycoordinate, entranceDirection):
-        super().__init__(xcoordinate, ycoordinate)
-        self.entranceDirection: EntranceDirection = entranceDirection
+        super().__init__(xcoordinate, ycoordinate, entranceDirection)
 
 
 class EntranceDirection(Enum):
