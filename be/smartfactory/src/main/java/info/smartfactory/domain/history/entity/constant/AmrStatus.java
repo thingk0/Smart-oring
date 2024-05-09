3a63de89be508eea08d52064ac022d0f674c64f3
Hgstudio <tndms0308@naver.com>
2024-05-09 13:11:33 +0900

    ♻️ refactor: AmrStatus STOPPED 대신 BOTTLENECK 구체적 명시

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
@@ -2,7 +2,7 @@ package info.smartfactory.domain.history.entity.constant;
 
 public enum AmrStatus {
     PROCESSING,         // 미션 수행 중
-    STOPPED,        // 미션 수행 중 멈춤
+    BOTTLENECK,// 미션 수행 중 멈춤
     CHARGING,       // 충전 중
     ERROR,          // 에러 발생
     DISCHARGING     // 방전
