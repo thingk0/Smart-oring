113590df2b525a13153f007457c7f70bc3b27081
고명성 <audtjd9080@naver.com>
2024-05-10 13:07:42 +0900

    🔧 config: Kafka 설정 개선 및 프로듀서 설정 추가
    - `KafkaConfig` 클래스:
      - Kafka 프로듀서 설정 추가
      - Kafka `MissionKafkaDTO` 프로듀서 및 컨슈머 설정 업데이트
      - 포맷팅 및 의존성 정리

--- be/smartfactory/src/main/java/info/smartfactory/global/config/KafkaConfig.java
+++ be/smartfactory/src/main/java/info/smartfactory/global/config/KafkaConfig.java
@@ -3,15 +3,22 @@ package info.smartfactory.global.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import info.smartfactory.domain.mission.dto.MissionKafkaDTO;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
 public class KafkaConfig {
@@ -27,7 +34,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092, localhost:9093, localhost:9094");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:10000, localhost:10001, localhost:10002");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "main-consumer-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -36,9 +43,24 @@ public class KafkaConfig {
     }
 
     @Bean
+    public ProducerFactory<String, MissionKafkaDTO> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:10000, localhost:10001, localhost:10002");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
+    }
+
+    @Bean
+    public KafkaTemplate<String, MissionKafkaDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
