d3825738fefc103bba4f09fabc5355090a9e1a3a
고명성 <audtjd9080@naver.com>
2024-05-10 09:28:00 +0900

    ♻️ refactor: AmrStatus 열거형 상수 정렬 및 주석 수정
    - BOTTLENECK 상수 위치 조정 및 주석 수정

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/entity/constant/AmrStatus.java
@@ -1,8 +1,8 @@
 package info.smartfactory.domain.history.entity.constant;
 
 public enum AmrStatus {
-    PROCESSING,         // 미션 수행 중
-    BOTTLENECK,// 미션 수행 중 멈춤
+    PROCESSING,     // 미션 수행 중
+    BOTTLENECK,     // 미션 수행 중 멈춤
     CHARGING,       // 충전 중
     ERROR,          // 에러 발생
     DISCHARGING     // 방전
