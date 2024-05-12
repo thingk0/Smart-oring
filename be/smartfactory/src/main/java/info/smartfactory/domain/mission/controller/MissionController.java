b94ece6890b7b04acce00a238c89b81cc58bba97
고명성 <audtjd9080@naver.com>
2024-05-12 17:48:57 +0900

    ✨ feat: 미션 이력 조회 엔드포인트 추가
    - 페이지네이션, 필터링 기능을 지원하여 AMR 유형, 시작/종료 시간, 병목 초과 시간을 기준으로 미션 이력을 조회
    - PageableDefault 를 통해 기본 페이지 크기를 설정하고, DateTimeFormat 어노테이션으로 시간 필드의 형식을 지정

--- be/smartfactory/src/main/java/info/smartfactory/domain/mission/controller/MissionController.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/controller/MissionController.java
@@ -2,12 +2,20 @@ package info.smartfactory.domain.mission.controller;
 
 import info.smartfactory.domain.mission.service.MissionService;
 import info.smartfactory.domain.mission.service.dto.MissionDto;
+import info.smartfactory.domain.mission.service.dto.MissionHistoryDto;
 import info.smartfactory.global.result.ResultResponse;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +24,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class MissionController {
 
     private final MissionService missionService;
+
+    @GetMapping
+    public ResultResponse<Page<MissionHistoryDto>> getMissionHistory(
+        @RequestParam(required = false) String amrType,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
+        @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime,
+        @RequestParam(required = false) Integer bottleneckSeconds,
+        @PageableDefault(size = 20) Pageable pageable
+    ) {
+        Page<MissionHistoryDto> missionHistoryPage = missionService.getMissionHistories(pageable, amrType, startTime, endTime, bottleneckSeconds);
+        return ResultResponse.res(HttpStatus.OK, "success", missionHistoryPage);
+    }
 
     @GetMapping("/{missionId}")
     public ResultResponse<MissionDto> getMissionInfo(@PathVariable(name = "missionId") Long missionId) {
