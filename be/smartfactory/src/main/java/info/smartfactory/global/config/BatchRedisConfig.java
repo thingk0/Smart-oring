57afccd9e780d42df19e0218521a33ba173e3f31
bluepeng9 <bluepeng9@gmail.com>
2024-05-13 02:55:52 +0900

    🐛 fix: redis 2개 안 쓰는 버그 수정

--- be/smartfactory/src/main/java/info/smartfactory/global/config/BatchRedisConfig.java
+++ be/smartfactory/src/main/java/info/smartfactory/global/config/BatchRedisConfig.java
@@ -2,14 +2,21 @@ package info.smartfactory.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.keyvalue.core.KeyValueTemplate;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisKeyValueTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "info.smartfactory.domain.history.repository.batch",
-        redisTemplateRef = "batchRedisTemplate")
+@EnableRedisRepositories(
+	basePackages = "info.smartfactory.domain.history.repository.batch",
+	redisTemplateRef = "batchRedisTemplate",
+	keyValueTemplateRef = "batchRedisKeyValueTemplate"
+)
 public class BatchRedisConfig {
     @Bean
     public LettuceConnectionFactory batchRedisConnectionFactory() {
@@ -22,4 +29,12 @@ public class BatchRedisConfig {
         template.setConnectionFactory(batchRedisConnectionFactory());
         return template;
     }
+
+	@Bean(name = "batchRedisKeyValueTemplate")
+	public KeyValueTemplate batchRedisKeyValueTemplate() {
+		return new RedisKeyValueTemplate(
+			new RedisKeyValueAdapter(batchRedisTemplate()),
+			new RedisMappingContext()
+		);
+	}
 }
--- be/smartfactory/src/main/java/info/smartfactory/global/config/RedisConfig.java
+++ be/smartfactory/src/main/java/info/smartfactory/global/config/RedisConfig.java
@@ -3,14 +3,21 @@ package info.smartfactory.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.keyvalue.core.KeyValueTemplate;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisKeyValueTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "info.smartfactory.domain.history.repository.live",
-        redisTemplateRef = "liveRedisTemplate")
+@EnableRedisRepositories(
+    basePackages = "info.smartfactory.domain.history.repository.live",
+    redisTemplateRef = "liveRedisTemplate",
+    keyValueTemplateRef = "liveRedisKeyValueTemplate"
+)
 public class RedisConfig {
 
     // 로봇 최신 상태 저장
@@ -25,6 +32,14 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         return template;
+    }
+
+    @Bean(name = "liveRedisKeyValueTemplate")
+    public KeyValueTemplate liveRedisKeyValueTemplate() {
+        return new RedisKeyValueTemplate(
+            new RedisKeyValueAdapter(redisTemplate()),
+            new RedisMappingContext()
+        );
     }
 }
 
