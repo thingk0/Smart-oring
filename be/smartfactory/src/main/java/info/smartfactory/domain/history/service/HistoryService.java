a2cab31275aa1c2c716a040d199f76d67c429f31
고명성 <audtjd9080@naver.com>
2024-05-12 17:40:30 +0900

    ♻️ refactor: 임포트 및 생성자 코드 정리
    - 불필요한 import 문 제거
    - 생성자의 @Autowired 어노테이션 제거 및 코드 정리

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/service/HistoryService.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/service/HistoryService.java
@@ -1,61 +1,57 @@
 package info.smartfactory.domain.history.service;
 
+
 import info.smartfactory.domain.bottleneck.service.BottleneckDto;
 import info.smartfactory.domain.bottleneck.service.BottleneckService;
+import info.smartfactory.domain.history.dto.AmrHistoryLog;
 import info.smartfactory.domain.history.entity.constant.AmrStatus;
 import info.smartfactory.domain.history.repository.BatchAmrInfoRedisDto;
 import info.smartfactory.domain.history.repository.CurrentAmrInfoRedisDto;
 import info.smartfactory.domain.history.repository.batch.BatchAmrRedisRepository;
+import info.smartfactory.domain.history.repository.live.CurrentAmrRedisRepository;
 import info.smartfactory.domain.history.service.Mapper.AmrHistoryMapper;
 import info.smartfactory.domain.history.service.Mapper.CurrentAmrMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import info.smartfactory.domain.history.dto.AmrHistoryLog;
-import info.smartfactory.domain.history.repository.live.CurrentAmrRedisRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class HistoryService {
 
-	private final RedisTemplate<String, Object> liveRedisTemplate;
-	private final RedisTemplate<String, Object> batchRedisTemplate;
-	private final CurrentAmrRedisRepository currentAmrRedisRepository;
-	private final BatchAmrRedisRepository batchAmrRedisRepository;
-	private final BottleneckService bottleneckService;
+    private final RedisTemplate<String, Object> liveRedisTemplate;
+    private final RedisTemplate<String, Object> batchRedisTemplate;
+    private final CurrentAmrRedisRepository currentAmrRedisRepository;
+    private final BatchAmrRedisRepository batchAmrRedisRepository;
+    private final BottleneckService bottleneckService;
 
-	@Autowired
-	public HistoryService(@Qualifier("liveRedisTemplate") RedisTemplate<String, Object> redisTemplate,
-					   @Qualifier("batchRedisTemplate") RedisTemplate<String, Object> batchRedisTemplate,
-						   CurrentAmrRedisRepository currentAmrRedisRepository,
-                           BatchAmrRedisRepository batchAmrRedisRepository,
-						   BottleneckService bottleneckService) {
-		this.liveRedisTemplate = redisTemplate;
-		this.batchRedisTemplate = batchRedisTemplate;
-		this.currentAmrRedisRepository = currentAmrRedisRepository;
-		this.batchAmrRedisRepository = batchAmrRedisRepository;
-		this.bottleneckService = bottleneckService;
-	}
+    public HistoryService(@Qualifier("liveRedisTemplate") RedisTemplate<String, Object> redisTemplate,
+                          @Qualifier("batchRedisTemplate") RedisTemplate<String, Object> batchRedisTemplate,
+                          CurrentAmrRedisRepository currentAmrRedisRepository,
+                          BatchAmrRedisRepository batchAmrRedisRepository,
+                          BottleneckService bottleneckService) {
+        this.liveRedisTemplate = redisTemplate;
+        this.batchRedisTemplate = batchRedisTemplate;
+        this.currentAmrRedisRepository = currentAmrRedisRepository;
+        this.batchAmrRedisRepository = batchAmrRedisRepository;
+        this.bottleneckService = bottleneckService;
+    }
 
-	public void saveHistory(AmrHistoryLog amrHistoryLog) {
-		// 병목 기간 저장
+    public void saveHistory(AmrHistoryLog amrHistoryLog) {
+        // 병목 기간 저장
 
-		Optional<CurrentAmrInfoRedisDto> previous = currentAmrRedisRepository.findById(amrHistoryLog.amrId().toString());
+        Optional<CurrentAmrInfoRedisDto> previous = currentAmrRedisRepository.findById(amrHistoryLog.amrId().toString());
 
-		long period = 0L;
+        long period = 0L;
 
-		if (previous.isPresent()) {
-			CurrentAmrInfoRedisDto previousAmrInfo = previous.get();
+        if (previous.isPresent()) {
+            CurrentAmrInfoRedisDto previousAmrInfo = previous.get();
 
             if (!(amrHistoryLog.amrStatus() == AmrStatus.BOTTLENECK) && previousAmrInfo.getStopPeriod() > 0L) {
                 bottleneckService.addBottleneckData(BottleneckDto.builder()
                                                                  .missionId(amrHistoryLog.missionId())
-															     .amrId(amrHistoryLog.amrId())
+                                                                 .amrId(amrHistoryLog.amrId())
                                                                  .xCoordinate(amrHistoryLog.xCoordinate())
                                                                  .yCoordinate(amrHistoryLog.yCoordinate())
                                                                  .bottleneckPeriod(previousAmrInfo.getStopPeriod())
@@ -63,45 +59,45 @@ public class HistoryService {
                                                                  .build());
             }
 
-			if (amrHistoryLog.amrStatus() == AmrStatus.BOTTLENECK) {
-				period = previousAmrInfo.getStopPeriod() + 1L;
-			}
-		}else{
-			if (amrHistoryLog.amrStatus() == AmrStatus.BOTTLENECK) {
-				period += 1L;
-			}
-		}
+            if (amrHistoryLog.amrStatus() == AmrStatus.BOTTLENECK) {
+                period = previousAmrInfo.getStopPeriod() + 1L;
+            }
+        } else {
+            if (amrHistoryLog.amrStatus() == AmrStatus.BOTTLENECK) {
+                period += 1L;
+            }
+        }
 
-		// redis에 amr 실시간 위치 저장
+        // redis에 amr 실시간 위치 저장
 
-		CurrentAmrInfoRedisDto redisDto = CurrentAmrMapper.INSTANCE.mapToRedisDto(amrHistoryLog);
+        CurrentAmrInfoRedisDto redisDto = CurrentAmrMapper.INSTANCE.mapToRedisDto(amrHistoryLog);
 
-		CurrentAmrMapper.INSTANCE.setStopPeriod(amrHistoryLog, redisDto, period);
+        CurrentAmrMapper.INSTANCE.setStopPeriod(amrHistoryLog, redisDto, period);
 
-		currentAmrRedisRepository.save(redisDto);
+        currentAmrRedisRepository.save(redisDto);
 
-		// redis에 amr 이력 저장
+        // redis에 amr 이력 저장
 
-		BatchAmrInfoRedisDto amrHistoryDto = AmrHistoryMapper.INSTANCE.mapToRedisDto(amrHistoryLog);
+        BatchAmrInfoRedisDto amrHistoryDto = AmrHistoryMapper.INSTANCE.mapToRedisDto(amrHistoryLog);
 
-		AmrHistoryMapper.INSTANCE.setStopPeriod(amrHistoryLog, amrHistoryDto, period);
+        AmrHistoryMapper.INSTANCE.setStopPeriod(amrHistoryLog, amrHistoryDto, period);
 
-		batchAmrRedisRepository.save(amrHistoryDto);
-	}
+        batchAmrRedisRepository.save(amrHistoryDto);
+    }
 
-	// amr 현재 위치 가져오기
+    // amr 현재 위치 가져오기
 
-	public List<CurrentAmrInfoRedisDto> getRecentRobotStates() {
-		List<CurrentAmrInfoRedisDto> all = currentAmrRedisRepository.findAll();
-		all.forEach(System.out::println);
-		return all;
-	}
+    public List<CurrentAmrInfoRedisDto> getRecentRobotStates() {
+        List<CurrentAmrInfoRedisDto> all = currentAmrRedisRepository.findAll();
+        all.forEach(System.out::println);
+        return all;
+    }
 
-	// amr 이력 정보 가져오기
+    // amr 이력 정보 가져오기
 
-	public List<BatchAmrInfoRedisDto> getRobotHistories() {
-		List<BatchAmrInfoRedisDto> all = batchAmrRedisRepository.findAll();
-		all.forEach(System.out::println);
-		return all;
-	}
+    public List<BatchAmrInfoRedisDto> getRobotHistories() {
+        List<BatchAmrInfoRedisDto> all = batchAmrRedisRepository.findAll();
+        all.forEach(System.out::println);
+        return all;
+    }
 }
