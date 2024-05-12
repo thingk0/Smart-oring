d539b8de916d0dfd0a466ef2ae64a8aaa8455f4a
엄진식 <ladiescheong@naver.com>
2024-05-12 22:25:13 +0900

    ✨ feat: 가동률 이외 요소들 구현

--- frontend/src/entities/Analysis/ui/Dashboard.tsx
+++ frontend/src/entities/Analysis/ui/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { DashboardData } from '../types';
-import RadialBar from './RadialBar';
+import ErrorRate from './ErrorRate';
+import OperaingRate from './OperatingRate';
+import UsageRate from './UsageRate';
 
 type DashboardProps = {
   resource: {
@@ -15,10 +17,46 @@ function Dashboard({ resource }: DashboardProps) {
     <>
       <h1>Dashboard</h1>
       <div>
-        <RadialBar data={data[0].totalUsagePercent} />
+        {/* <div>
+          <div>미션 처리량</div>
+          <div>
+            <OperaingRate data={data[0].totalUsagePercent} />
+            <UsageRate data={data[0].amrUsagePercent} />
+            <ErrorRate data={data[0].amrErrorPercent} />
+          </div>
+        </div> */}
+        <div>
+          <div>
+            <h3>시간</h3>
+          </div>
+          <div>
+            <h3>오늘 생산량</h3>
+            <p>{data[0].todayTotalMissionCount}</p>
+          </div>
+          <div>
+            <h3>실시간 에러</h3>
+            {data[0].realtimeError.map(({ amrId, missionId }) => {
+              return (
+                <p>
+                  {amrId}번 기기 - {missionId} 미션 수행 시 에러
+                </p>
+              );
+            })}
+          </div>
+          <div>
+            <h3>실시간 병목</h3>
+            {data[0].realtimeBottleneck.map(
+              ({ amrId, xcoordinate, ycoordinate }) => {
+                return (
+                  <p>
+                    {amrId}번 기기 - ({xcoordinate}, {ycoordinate}) 좌표 병목
+                  </p>
+                );
+              }
+            )}
+          </div>
+        </div>
       </div>
-      {/* <div>AMR 사용률</div>
-      <div>AMR 에러율</div> */}
     </>
   );
 }
