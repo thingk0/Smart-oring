55e4ed837db57b59dc09dab147dc0c46b3eca6af
고명성 <audtjd9080@naver.com>
2024-05-12 17:16:51 +0900

    ♻️ refactor: AmrHistoryLog DTO 코드 정리 및 정렬
    - 불필요한 import 문 제거
    - 필드 정렬 방식을 개선하여 가독성 향상

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/dto/AmrHistoryLog.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/dto/AmrHistoryLog.java
@@ -1,25 +1,20 @@
 package info.smartfactory.domain.history.dto;
 
-import info.smartfactory.domain.amr.entity.Amr;
 import info.smartfactory.domain.history.entity.AmrHistory;
 import info.smartfactory.domain.history.entity.constant.AmrStatus;
-import info.smartfactory.domain.mission.entity.Mission;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * DTO for {@link AmrHistory}
  */
-public record AmrHistoryLog(
-        Long missionId,
-        Long amrId,
-        Integer battery,
-        Integer xCoordinate,
-        Integer yCoordinate,
-        List<Integer[]> amrRoute,
-        AmrStatus amrStatus,
-        LocalDateTime amrHistoryCreatedAt
-        ) {
+public record AmrHistoryLog(Long missionId,
+                            Long amrId,
+                            Integer battery,
+                            Integer xCoordinate,
+                            Integer yCoordinate,
+                            List<Integer[]> amrRoute,
+                            AmrStatus amrStatus,
+                            LocalDateTime amrHistoryCreatedAt) {
 
 }
