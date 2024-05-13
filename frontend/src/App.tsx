fdc75a526510d93315a4a0472f4451f5faaf3f40
김예지 <fpssj@naver.com>
2024-05-13 14:15:22 +0900

    ✨ feat: 스피드 다이얼 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import BasicSpeedDial from 'widgets/floating/BasicSpeedDial';
 import Renderer from './bases/Renderer';
 
 function App() {
   return (
     <>
       <Renderer />
+      <BasicSpeedDial />
     </>
   );
 }
