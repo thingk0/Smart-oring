ce7e328c7099878c30acc27cb1880339c89ac0e2
고명성 <audtjd9080@naver.com>
2024-05-13 14:09:07 +0900

    ♻️ refactor: AMR 전체 목록 반환 메서드 수정.
    - getAmrs 메소드를 getAmrList로 변경하고, 반환 타입을 AmrInfoDto 목록으로 수정.

--- be/smartfactory/src/main/java/info/smartfactory/domain/amr/service/AmrService.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/amr/service/AmrService.java
@@ -1,30 +1,22 @@
 package info.smartfactory.domain.amr.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import info.smartfactory.domain.amr.entity.Amr;
 import info.smartfactory.domain.amr.repository.AmrRepository;
+import info.smartfactory.domain.amr.service.dto.AmrInfoDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AmrService {
 
     private final AmrRepository amrRepository;
-    private final AmrMapper amrMapper;
 
-    public List<AmrDto> getAmrs() {
-        List<Amr> amrs = amrRepository.findAll();
-
-        List<AmrDto> amrDtos = new ArrayList<>();
-        for (Amr amr : amrs) {
-            AmrDto dto = amrMapper.toDto(amr);
-            amrDtos.add(dto);
-        }
-        return amrDtos;
+    @Transactional(readOnly = true)
+    public List<AmrInfoDto> getAmrList() {
+        return amrRepository.fetchAmrInfoDtoList();
     }
 
     public Amr findAmrByIdOrThrow(Long amrId) {
