1eee3b8f67478e8ebbf4b6662214f5568c989692
고명성 <audtjd9080@naver.com>
2024-05-13 14:20:23 +0900

    ⚡ improve: 스프링 부트 애플리케이션 캐싱 기능 활성화

--- be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
+++ be/smartfactory/src/main/java/info/smartfactory/SmartFactoryApplication.java
@@ -2,11 +2,13 @@ package info.smartfactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka
+@EnableCaching
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
