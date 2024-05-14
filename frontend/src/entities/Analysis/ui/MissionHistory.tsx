97828c8c13f72e57e9e0a96b01874c19af5ed7b8
엄진식 <ladiescheong@naver.com>
2024-05-14 22:33:01 +0900

    💚 design: Mission 컴포넌트 임시 UI 제작 및 예시 데이터 선정
    rebase - canvas-capture 라이브러리 제거
    
    interactive rebase in progress; onto 6763592
    Last commands done (15 commands done):
       pick 526c90e 💄 style: Dashboard 컴포넌트 UI, Flex에서 Grid로 변경
       pick c18f585 💚 design: Mission 컴포넌트 임시 UI 제작 및 예시 데이터 선정
    Next commands to do (10 remaining commands):
       pick b8fbb7b 💚 design: Grid 속성 4x3으로 변경
       pick a6bff08 💚 design: Typography 변경 및 Dashboard 스타일 변경
    You are currently rebasing branch 'fe/feat/analysis' on '6763592'.

--- frontend/src/entities/Analysis/ui/MissionHistory.tsx
+++ frontend/src/entities/Analysis/ui/MissionHistory.tsx
@@ -1,13 +1,129 @@
+import { Button, Typography } from '@mui/material';
 import useMissionStore from '../store/useMissionStore';
+import styles from './Analysis.module.css';
+import ReplayIcon from '@mui/icons-material/Replay';
+import ReactApexChart from 'react-apexcharts';
 
 function MissionHistory() {
   const history = useMissionStore(state => state.missionHistory);
+  // console.log(history);
+
+  // console.log(history?.processing_time);
+  // console.log(history?.bottleneck_time);
+  // console.log(history?.error_time);
+
+  const series = [
+    history?.processing_time,
+    history?.bottleneck_time,
+    history?.error_time,
+  ];
+  const options = {
+    chart: {
+      type: 'donut',
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: 'bottom',
+          },
+        },
+      },
+    ],
+  };
+
+  const timelineSeries = [
+    {
+      data: [
+        {
+          x: 'Code',
+          y: [
+            new Date('2019-03-02').getTime(),
+            new Date('2019-03-04').getTime(),
+          ],
+        },
+        {
+          x: 'Test',
+          y: [
+            new Date('2019-03-04').getTime(),
+            new Date('2019-03-08').getTime(),
+          ],
+        },
+        {
+          x: 'Validation',
+          y: [
+            new Date('2019-03-08').getTime(),
+            new Date('2019-03-12').getTime(),
+          ],
+        },
+        {
+          x: 'Deployment',
+          y: [
+            new Date('2019-03-12').getTime(),
+            new Date('2019-03-18').getTime(),
+          ],
+        },
+      ],
+    },
+  ];
+  const timelineOptions = {
+    chart: {
+      height: 350,
+      type: 'rangeBar',
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    xaxis: {
+      type: 'datetime',
+    },
+  };
 
   return (
-    <>
-      <h3>미션 히스토리</h3>
-      <p>{history?.total_execution_time}</p>
-    </>
+    history && (
+      <div
+        className={`${styles.component_background} ${styles.mission_grid_column}`}
+      >
+        <div className={`${styles.flex_center} ${styles.margin_bottom}`}>
+          <div>
+            <Typography component="h2" variant="h5">
+              대충 AMR 번호랑 미션 번호
+            </Typography>
+            <Typography component="p" variant="body2">
+              대충 시작 시간과 종료 시간
+            </Typography>
+          </div>
+
+          <Button variant="contained" endIcon={<ReplayIcon />}>
+            Replay
+          </Button>
+        </div>
+        <div className={styles.flex}>
+          <div>
+            <Typography component="p" variant="h5">
+              main graph
+            </Typography>
+            <ReactApexChart type="donut" series={series} options={options} />
+          </div>
+          <div>
+            <Typography component="p" variant="h5">
+              line graph
+            </Typography>
+            <ReactApexChart
+              type="rangeBar"
+              series={timelineSeries}
+              options={timelineOptions}
+            />
+          </div>
+        </div>
+      </div>
+    )
   );
 }
 
