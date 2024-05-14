089b1897e46b3219cdafb7d969f6e7f5930c9bb1
엄진식 <ladiescheong@naver.com>
2024-05-14 15:01:10 +0900

    💄 style: mui의 tabs를 이용해 top navigation 스타일 변경

--- frontend/src/entities/Analysis/ui/index.tsx
+++ frontend/src/entities/Analysis/ui/index.tsx
@@ -6,31 +6,37 @@ import Mission from './Mission';
 
 import { LoadData } from '@shared/api';
 import styles from './Analysis.module.css';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
 
 function Analysis() {
-  const [type, setType] = useState('dashboard');
+  const [type, setType] = useState(0);
 
-  const onClickHandler = (val: string) => {
-    setType(val);
+  const onChangeHandler = (event: React.SyntheticEvent, newValue: number) => {
+    setType(newValue);
   };
 
   return (
     <div className={styles.wrap}>
       <div>
-        <p>top navigation</p>
-        <ul>
-          <li onClick={() => onClickHandler('dashboard')}>Dashboard</li>
-          <li onClick={() => onClickHandler('mission')}>Mission</li>
-          <li onClick={() => onClickHandler('heatmap')}>Heatmap</li>
-        </ul>
+        <Typography>top navigation</Typography>
+
+        <Tabs
+          value={type}
+          onChange={onChangeHandler}
+          aria-label="Analysis Tab Nagivation"
+        >
+          <Tab label="dashboard" />
+          <Tab label="mission" />
+          <Tab label="heatmap" />
+        </Tabs>
       </div>
 
       <Suspense fallback={<h1>Loading...</h1>}>
-        {type === 'dashboard' && (
+        {type === 0 && (
           <Dashboard resource={LoadData(import.meta.env.VITE_DASHBOARD_URL)} />
         )}
-        {type === 'mission' && <Mission />}
-        {type === 'heatmap' && (
+        {type === 1 && <Mission />}
+        {type === 2 && (
           <Heatmap resource={LoadData(import.meta.env.VITE_HEATMAP_URL)} />
         )}
       </Suspense>
