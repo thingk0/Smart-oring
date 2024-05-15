3728d16cdab0564309ff85f8ba128e41369f8fea
bluepeng9 <bluepeng9@gmail.com>
2024-05-15 17:42:49 +0900

    ✨ feat: UTC Instant 추가

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/dto/AmrHistoryLog.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/dto/AmrHistoryLog.java
@@ -1,8 +1,11 @@
 package info.smartfactory.domain.history.dto;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import info.smartfactory.domain.history.entity.AmrHistory;
@@ -12,13 +15,27 @@ import info.smartfactory.domain.history.entity.constant.AmrStatus;
  * DTO for {@link AmrHistory}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record AmrHistoryLog(Long missionId,
-                            Long amrId,
-                            Integer battery,
-                            Integer xCoordinate,
-                            Integer yCoordinate,
-                            List<Integer[]> amrRoute,
-                            AmrStatus amrStatus,
-                            LocalDateTime amrHistoryCreatedAt) {
+public record AmrHistoryLog(
+	Long missionId,
+	Long amrId,
+	Integer battery,
+	Integer xCoordinate,
+	Integer yCoordinate,
+	List<Integer[]> amrRoute,
+	AmrStatus amrStatus,
+	@Deprecated
+	LocalDateTime amrHistoryCreatedAt,
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX")
+	Instant amrHistoryCreatedAInstant
+) {
+
+	public LocalDateTime amrHistoryCreatedAt() {
+		Instant instant = amrHistoryCreatedAInstant();
+		if (instant == null) {
+			return null;
+		}
+		return LocalDateTime.ofInstant(instant, OffsetDateTime.now().toZonedDateTime().getZone());
+	}
 
 }
