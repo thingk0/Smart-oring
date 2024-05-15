35ef8d13b7216cf4000edba48fd3608710ac9fdb
고명성 <audtjd9080@naver.com>
2024-05-15 16:45:28 +0900

    ✨ feat: AMR 상태 분석 기능 확장 및 데이터 접근 최적화
    - `fetchMissionAnalysisInfo` 메서드의 반환 타입을 `MissionInfoDto`에서 `MissionExecutionTimeAnalysisDto`로 변경하여 더 상세한 미션 실행 시간 분석을 제공
    - 새로운 메서드 `fetchAmrStatusWithTime` 추가하여, 특정 미션에 대한 AMR 상태와 시간 정보를 시간 순으로 리스트 형태로 제공
    - QueryDSL의 `Projections`를 활용하여 데이터 매핑 과정을 간소화, 결과적으로 코드의 효율성 및 가독성을 향상

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/repository/custom/AmrHistoryRepositoryImpl.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/repository/custom/AmrHistoryRepositoryImpl.java
@@ -1,11 +1,14 @@
 package info.smartfactory.domain.history.repository.custom;
 
+import static info.smartfactory.domain.amr.entity.QAmr.amr;
 import static info.smartfactory.domain.history.entity.QAmrHistory.amrHistory;
 
-import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import info.smartfactory.domain.history.entity.constant.AmrStatus;
-import info.smartfactory.domain.mission.service.dto.MissionInfoDto;
+import info.smartfactory.domain.mission.service.dto.MissionAnalysisDto.AmrStatusWithTime;
+import info.smartfactory.domain.mission.service.dto.MissionAnalysisDto.MissionExecutionTimeAnalysisDto;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,34 +17,32 @@ public class AmrHistoryRepositoryImpl implements AmrHistoryRepositoryCustom {
     private final JPAQueryFactory query;
 
     @Override
-    public MissionInfoDto fetchMissionAnalysisInfo(Long missionId) {
-        Tuple result = query
-            .select(amrHistory.count(),
-                    amrHistory.amrStatus.when(AmrStatus.valueOf("PROCESSING")).then(1).otherwise(0).sum(),
-                    amrHistory.amrStatus.when(AmrStatus.valueOf("BOTTLENECK")).then(1).otherwise(0).sum(),
-                    amrHistory.amrStatus.when(AmrStatus.valueOf("CHARGING")).then(1).otherwise(0).sum(),
-                    amrHistory.amrStatus.when(AmrStatus.valueOf("ERROR")).then(1).otherwise(0).sum(),
-                    amrHistory.amrStatus.when(AmrStatus.valueOf("DISCHARGING")).then(1).otherwise(0).sum())
+    public MissionExecutionTimeAnalysisDto fetchMissionAnalysisInfo(Long missionId) {
+        return query
+            .select(Projections.constructor(MissionExecutionTimeAnalysisDto.class,
+                                            amr.amrCode,
+                                            amrHistory.mission.id,
+                                            amrHistory.count(),
+                                            amrHistory.amrStatus.when(AmrStatus.PROCESSING).then(1).otherwise(0).sum(),
+                                            amrHistory.amrStatus.when(AmrStatus.BOTTLENECK).then(1).otherwise(0).sum(),
+                                            amrHistory.amrStatus.when(AmrStatus.CHARGING).then(1).otherwise(0).sum(),
+                                            amrHistory.amrStatus.when(AmrStatus.ERROR).then(1).otherwise(0).sum(),
+                                            amrHistory.amrStatus.when(AmrStatus.DISCHARGING).then(1).otherwise(0).sum()))
             .from(amrHistory)
+            .leftJoin(amrHistory.amr, amr)
             .where(amrHistory.mission.id.eq(missionId))
             .groupBy(amrHistory.mission.id)
             .fetchOne();
+    }
 
-        if (result != null) {
-            return new MissionInfoDto(
-                result.get(0, Integer.class),        // Total count
-                result.get(1, Integer.class),       // PROCESSING sum
-                result.get(2, Integer.class),       // BOTTLENECK sum
-                result.get(3, Integer.class),       // CHARGING sum
-                result.get(4, Integer.class),       // ERROR sum
-                result.get(5, Integer.class),       // DISCHARGING sum
-                query.select(amrHistory.amrStatus)    // List<AmrStatus>
-                     .from(amrHistory)
-                     .where(amrHistory.mission.id.eq(missionId))
-                     .fetch()
-            );
-        } else {
-            throw new IllegalArgumentException("미션 ID에 대한 정보가 없습니다: " + missionId);
-        }
+    @Override
+    public List<AmrStatusWithTime> fetchAmrStatusWithTime(Long missionId) {
+        return query.select(Projections.constructor(AmrStatusWithTime.class,
+                                                    amrHistory.amrStatus,
+                                                    amrHistory.amrHistoryCreatedAt))
+                    .from(amrHistory)
+                    .where(amrHistory.mission.id.eq(missionId))
+                    .orderBy(amrHistory.amrHistoryCreatedAt.asc())
+                    .fetch();
     }
 }
