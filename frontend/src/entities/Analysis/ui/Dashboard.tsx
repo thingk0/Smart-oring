e377aeeb0ea89d631a96bab70d4983fb2fde8583
엄진식 <ladiescheong@naver.com>
2024-05-15 13:38:29 +0900

    💚 design: Grid 속성 4x3으로 변경

--- frontend/src/entities/Analysis/ui/Dashboard.tsx
+++ frontend/src/entities/Analysis/ui/Dashboard.tsx
@@ -22,6 +22,10 @@ function Dashboard({ resource }: DashboardProps) {
   // 따라서 tanstack-query 써야할 것 같은데
   // 부모가 리렌더링되니까 자식도 리렌더링 될거 같은데?
 
+  // 현재 시간을 빼고 4x3 형태로 수정하기
+
+  // Typography 들어간 부분 싹 다 수정하기
+
   return (
     <>
       {/* grid로 변경 */}
@@ -30,13 +34,13 @@ function Dashboard({ resource }: DashboardProps) {
           yesterday={data[0].yesterdayOutputGraph}
           today={data[0].todayOutputGraph}
         />
-        <CurrentTime />
-        <TodayOutput data={data[0].todayTotalOutput} />
+        {/* <CurrentTime /> */}
         <RealTimeError data={data[0].realtimeError} />
+        <RealTimeBottleneck data={data[0].realtimeBottleneck} />
+        <TodayOutput data={data[0].todayTotalOutput} />
         <OperatingRate data={data[0].totalUsagePercent} />
         <UsageRate data={data[0].amrUsagePercent} />
         <ErrorRate data={data[0].amrErrorPercent} />
-        <RealTimeBottleneck data={data[0].realtimeBottleneck} />
       </div>
     </>
   );
