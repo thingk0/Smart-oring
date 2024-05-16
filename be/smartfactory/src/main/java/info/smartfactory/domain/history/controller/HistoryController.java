c37a9bdc5ff341f858d788ad812cba5e5ad87340
Hgstudio <tndms0308@naver.com>
2024-05-16 09:21:04 +0900

    ♻️ refactor: getMapping 경로 수정 및 병목 리플레이 구현 중

--- be/smartfactory/src/main/java/info/smartfactory/domain/history/controller/HistoryController.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/history/controller/HistoryController.java
@@ -1,17 +1,20 @@
 package info.smartfactory.domain.history.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import info.smartfactory.domain.history.service.dto.ReplayDto;
 import info.smartfactory.domain.history.service.RealtimeAmrDto;
-import org.json.JSONException;
+import info.smartfactory.domain.history.service.dto.AmrHistoryDto;
+import info.smartfactory.domain.history.service.dto.ReplayDto;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.format.annotation.DateTimeFormat;
+import info.smartfactory.domain.history.service.RealtimeAmrDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import info.smartfactory.domain.history.dto.BatchAmrInfoRedisDto;
+import info.smartfactory.domain.history.dto.CurrentAmrInfoRedisDto;
 import info.smartfactory.domain.history.service.HistoryService;
 import info.smartfactory.global.result.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/histories")
 public class HistoryController {
 
     final private HistoryService historyService;
@@ -34,18 +38,28 @@ public class HistoryController {
 
     // amr 전체 이력 저장
     @Operation(summary = "로봇 기록 전체 조회")
-    @GetMapping("/histories/amr")
+    @GetMapping("/amr")
     public ResponseEntity<?> getRobotHistories() {
         List<BatchAmrInfoRedisDto> result = historyService.getRobotHistories();
         return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), result));
     }
 
-    @Operation(summary = "분석웹 Mission페이지 미션 리플레이")
-    @GetMapping("/histories/replay/{missionId}")
+    @Operation(summary = "분석웹 미션 전체 리플레이")
+    @GetMapping("/replay/{missionId}")
     public ResultResponse<?> getReplay(@PathVariable(name = "missionId") Long missionId) {
         List<ReplayDto> replayInfo = historyService.getReplay(missionId);
 
         return ResultResponse.res(HttpStatus.OK, "success", replayInfo);
+    }
+
+    @Operation(summary = "분석웹 특정 병목 리플레이")
+    @GetMapping("/replay/bottleneck")
+    public ResultResponse<?> getReplayBottleneck(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime) {
+//        historyService.getBottleneckReplay(startTime, endTime);
+
+        return ResultResponse.res(HttpStatus.OK, "success");
     }
 
 }
