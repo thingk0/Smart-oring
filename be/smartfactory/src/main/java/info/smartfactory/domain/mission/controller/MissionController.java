f33018f834a15b4dae32cf5f0b6b7643f77bc771
bluepeng9 <bluepeng9@gmail.com>
2024-05-17 15:47:09 +0900

    ✨ feat: GetMapping PostMapping으로 수정

--- be/smartfactory/src/main/java/info/smartfactory/domain/mission/controller/MissionController.java
+++ be/smartfactory/src/main/java/info/smartfactory/domain/mission/controller/MissionController.java
@@ -1,21 +1,29 @@
 package info.smartfactory.domain.mission.controller;
 
-import info.smartfactory.domain.mission.controller.response.AssignMissionRequest;
-import info.smartfactory.domain.mission.entity.Mission;
-import info.smartfactory.domain.mission.service.MissionService;
-import info.smartfactory.domain.mission.service.dto.MissionDto;
-import info.smartfactory.domain.mission.service.dto.MissionHistoryDto;
-import info.smartfactory.domain.mission.service.dto.MissionAnalysisDto;
-import info.smartfactory.global.result.ResultResponse;
 import java.time.LocalDateTime;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import info.smartfactory.domain.mission.controller.response.AssignMissionRequest;
+import info.smartfactory.domain.mission.entity.Mission;
+import info.smartfactory.domain.mission.service.MissionService;
+import info.smartfactory.domain.mission.service.dto.MissionAnalysisDto;
+import info.smartfactory.domain.mission.service.dto.MissionDto;
+import info.smartfactory.domain.mission.service.dto.MissionHistoryDto;
+import info.smartfactory.global.result.ResultResponse;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/missions")
@@ -48,7 +56,7 @@ public class MissionController {
         return ResultResponse.res(HttpStatus.OK, "success", missionInfo);
     }
 
-    @GetMapping("/assignment")
+    @PostMapping("/assignment")
     public ResultResponse<?> assignMission(@RequestBody AssignMissionRequest selectedNodeList) {
         Mission missionDto = missionService.assignMission(selectedNodeList);
 
